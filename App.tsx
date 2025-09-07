a2df9d0b1bd5f243be631927c4e55b121475e3bb
Metis08 <mahekaghija0808@gmail.com>
2025-09-07 11:46:03 +0530

    Added main.tsx entry file for Vite

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import About from "./pages/About";
-import NotFound from "./pages/NotFound";
+import Index from "./Index";
+
+import About from "./About";
+import NotFound from "./NotFound";
 
 const queryClient = new QueryClient();
 
